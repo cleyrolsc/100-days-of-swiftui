1518507738db519c4b386ce4b24b02f8fdb65811
Cleyrol Santana <cleyrolsc@gmail.com>
2025-06-19 22:33:57 -0400

    Finish the creation of the WeSplitApp

---
+++ day-17/WeSplit2/WeSplit2/ContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  WeSplit2
+//
+//  Created by Cleyrol Santana on 19/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var cost = 0.0
+    @State private var amountPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocus:Bool
+    
+    let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson: Double{
+        let peopleCount = Double(amountPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = cost / 100 * tipSelection
+        let grandTotal = cost + tipValue
+        let amountPerperson = grandTotal / peopleCount
+        
+        return amountPerperson
+    }
+    
+    var body: some View {
+        NavigationStack{
+            Form{
+                Section{
+                    TextField("Amount", value: $cost, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocus)
+                    
+                    Picker("Number of people", selection: $amountPeople){
+                        ForEach(2..<99){
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("How much do you want to tip?"){
+                    
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section{
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }.navigationTitle("WeSplit")
+                .toolbar{
+                    if amountIsFocus{
+                        Button("Done"){
+                            amountIsFocus = false
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ day-17/WeSplit2/WeSplit2/WeSplit2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit2App.swift
+//  WeSplit2
+//
+//  Created by Cleyrol Santana on 19/6/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
