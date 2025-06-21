6baaef11249f9b20b994d2a4c86cb4f07d193ca5
Cleyrol Santana <77045476+cleyrolsc@users.noreply.github.com>
2025-06-20 22:01:46 -0400

    Review the wesplit app, added a new section for practice and a header for two of those sections. Also, change the picker style for the tip percentage section

---
+++ day-18/WeSplit3/WeSplit3/WeSplit3App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit3App.swift
+//  WeSplit3
+//
+//  Created by Cleyrol Santana on 20/6/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ day-18/WeSplit3/WeSplit3Tests/WeSplit3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit3Tests.swift
+//  WeSplit3Tests
+//
+//  Created by Cleyrol Santana on 20/6/25.
+//
+
+import Testing
+@testable import WeSplit3
+
+struct WeSplit3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
