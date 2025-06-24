fde6a7d5f0e0e9ee9a85113b9ae722db733ea953
Cleyrol Santana <77045476+cleyrolsc@users.noreply.github.com>
2025-06-23 23:50:44 -0400

    Complete guess the flag game style and functionality

---
+++ day-21/GuessTheFlag2/GuessTheFlag2/GuessTheFlag2App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag2App.swift
+//  GuessTheFlag2
+//
+//  Created by Cleyrol Santana on 23/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ day-21/GuessTheFlag2/GuessTheFlag2Tests/GuessTheFlag2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag2Tests.swift
+//  GuessTheFlag2Tests
+//
+//  Created by Cleyrol Santana on 23/6/25.
+//
+
+import Testing
+@testable import GuessTheFlag2
+
+struct GuessTheFlag2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
